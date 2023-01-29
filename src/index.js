8a5fae93c4ec52525334d8f7d11813619f5b5775
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2023-01-29 13:07:14 +0300

    feat: added solution almost in 1 string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) { [...str].forEach(() => bracketsConfig.map(elem => elem.join('')).forEach(elem => str = str.replaceAll(elem, ''))); 
+return str === '';}
